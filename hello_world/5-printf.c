7c21a3add643357df1594279f2b2dfad64fbc2e1
Laurent Lacote <laurent.lacote@private.com>
2026-02-23 12:41:31 +0000

    C - HelloWorld - Exo 5 - Learning printf's ropes
    NOTES
    - Could have put directly the required string
        (more efficient in this context) but kept
        intermediate variable to learn how to use format.
    - Functionally putting the '\n' newline character
        in variable or within printf was the same.
        Not sure what the best practice would be
        for more complex use-cases.

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays a simple static text.
+ *
+ * Description: displays a fixed string using printf.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *text = "with proper grammar, but the outcome is a piece of art, ";
+
+	printf("%s \n", text);
+
+	return (0);
+
+}
