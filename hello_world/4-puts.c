b31c290482e322287ac695bcf07ab35ff4644b5b
Laurent Lacote <laurent.lacote@private.com>
2026-02-23 12:18:10 +0000

    Removing the endline

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *text = "\"Programming is like building a multilingual puzzle\n";
+	char *text = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
 
