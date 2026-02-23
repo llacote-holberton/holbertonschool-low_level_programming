815756ff447d8b823091cf81aa5b49d296753d40
Laurent Lacote <laurent.lacote@private.com>
2026-02-23 12:16:57 +0000

    Trying to fix exo 04

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main() 
+/*
+ * main - Displays a simple static text.
+ *
+ * Description: SAME AS ABOVE, SERIOUSLY.
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char *text="\"Programming is like building a multilingual puzzle\n";
-  puts (text);
-  return 0;
+	char *text = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(text);
+
+	return (0);
+
 }
