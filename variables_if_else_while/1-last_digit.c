41b8262eb6098bb3b560a658808d0815e6af2188
Laurent Lacôte <laurent.lacote@private.com>
2026-02-24 09:51:12 +0100

    C - BasicAlgorithms - Exo 1 - Ternary branching with a twist
    We start mixing different formats and defining variables.
    
    NOTES
    - Must beware of format choice in printf
        using %u (unsigned) led to unpredictable behaviour
        when paired with raw value of last digit (not absolute).
    - The mandatory compilation options are kinda annoying
        as they force separating declarations from assignations
        although I get the idea of "learning rigorously". ^^

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - says whether stored number is positive or negative.
+ * Description: determines the "signed range" of a n variable.
+ * Return: always 0 whatever resulting output is.
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+	char *text;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* @note needs to get "absolute value" to stay within exercise's frame. */
+	last_digit = abs(n % 10);
+	if (last_digit == 0)
+		text = "and is 0";
+	else if (last_digit <= 5)
+		text = "and is less than 6 and not 0";
+	else
+		text = "and is greater than 5";
+	/* Beware format choice, using "%u" leads to incoherent display. */
+	printf("Last digit of %d is %d %s\n", n, last_digit, text);
+
+	return (0);
+}
