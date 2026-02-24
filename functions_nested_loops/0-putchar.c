fd7070965cff71869de10a7e3c3e71b45b515086
Laurent Lacôte <laurent.lacote@private.com>
2026-02-24 15:23:24 +0100

    C - ComplexStructures - Exo 0 - Printing without standard libs.
    NOTES
    - I don't know how I could use a loop on a character array
        without using strlen which is forbidden since from a
        standard library.
    - When including a custom header choice need to be made on syntax
      * "mycustomheader.h" -> must be in same folder as source calling it.
      * <mycustomheader.h> -> must be in a "standard headers's folder".

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" and a new line.
+ * Description: uses provided external function to print characters.
+ *   Warning: requires file _putchar.c to be explicitely added
+ *   in the list of files to aggregate when running compilation.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
