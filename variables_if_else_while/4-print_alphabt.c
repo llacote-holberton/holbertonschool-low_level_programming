6d12eb286ca5f5816f952f6846c551386ee42b44
Laurent Lacôte <laurent.lacote@private.com>
2026-02-24 11:23:22 +0100

    C - BasicAlgorithms - Exo 4 THE RETURN
    Removed 2nd version as checker didn't allow
      use of strlen (yet).

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,26 +11,12 @@
  */
 int main(void)
 {
-	char *characters_set;
-	int set_size;
-	int pointer;
-	/*
-	 * Method 1 which would definitely work
-	 * @note mixing both writings to stress that compiler
-	 *   converts letter in its decimal ASCII value
-	 * int character_code;
-	 *
-	 * for (character_code = 97; character_code < 97 + 26; character_code++)
-	 * if(character_code != 101 && character_code != 'q')
-	 * putchar(character_code);*
-	 */
-	/* Method 2 which is probably less efficient, but just for fun */
-	characters_set = "abcdfghijklmnoprstuvwxyz";
-	set_size = (int) strlen(characters_set);
-	/* @note storing length in a variable to avoid redundant calls in loop */
-	/* @note requires casting into int because strlen gives a size_t type value */
-	for (pointer = 0; pointer < set_size; pointer++)
-		putchar(characters_set[pointer]);
+	int character_code;
+
+	for (character_code = 97; character_code < 97 + 26; character_code++)
+		if (character_code != 101 && character_code != 'q')
+		putchar(character_code);
 	putchar('\n');
+
 	return (0);
 }
