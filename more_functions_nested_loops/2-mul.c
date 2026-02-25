4c408862e6b19b5706c674af77d3422cdbbba1c6
Laurent Lacôte <laurent.lacote@private.com>
2026-02-25 18:06:07 +0100

    C - mF/mNL - Exo 02 - Fn to multiply 2 integers
    COMPILER
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-mul.c -o 2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Checks whether input char is a digit.
+ * @a: integer number.
+ * @b: integer number.
+ * Description: takes given character and checks
+ *   whether it is within digits character set
+ * Return: boolean (1 = success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,5 +7,7 @@ int _isupper(int c);
 /* Function defined in 1-isdigit.c */
 int _isdigit(int c);
 
+/* Function defined in 2-mul.c */
+int mul(int a, int b);
 
 #endif
