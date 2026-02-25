fdad63fe09ac5af1b9c2b59febdad9b81339b439
Laurent Lacôte <laurent.lacote@private.com>
2026-02-25 13:37:37 +0100

    C - F/NL - Exo 11 Full scope (from under AND from above)
    Changed the variable numbers as well
      as the "variation logic" to cover
      both "bottom-up" and "top-down" ranges.
    
    NOTE: made me learn about evaluation order
      when using variables several times in an expression. ^^
    
    RESOURCES
    - https://en.wikipedia.org/wiki/C_standard_library

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,28 +3,43 @@
 #include <stdio.h>
 
 /* Additional constant because I feel it's cleaner */
-static const int ceiling = 98;
+static const int end = 98;
 
 /**
- * print_to_98 - lists natural numbers up to 98
- * @floor: integer
+ * print_to_98 - lists natural numbers until reaching 98
+ * @start: integer
  * Description: sequences a numbering range
- *   between "floor" number (function parameter)
+ *   between "start" number (function parameter)
  *   and 98 (should be a constant?),
  *   separated by comma and space for readability.
  * Return: void
  */
-void print_to_98(int floor)
+void print_to_98(int start)
 {
-	while (floor < ceiling)
+	while (start != end)
 	{
-		printf("%d, ", floor);
+		printf("%d, ", start);
 		/*
 		 * I feel not good practice to not use dedicated counter
 		 *   but for this simple exercice deemed acceptable.
 		 */
-		floor++;
+		/*
+		 * DOES NOT WORK because "operation on start can be undefined"
+		 * -> I in|decrement AFTER re-affecting so I end up not changing.
+		 * start = (start > end) ? start-- : start++;
+		 * Even that is not "guaranteed to work" because technically
+		 *   this makes two affectations in a row on the same variable.
+		 * start = (start > end) ? --start : ++start;
+		 * At least in C versions anterior to C17, the ops order is not strict.
+		 */
+		start += (start > end) ? -1 : +1;
+		/*
+		 * ONLY the version above is reliable because
+		 * a) start is only (re)affected once
+		 * b) the condition is evaluated separately
+		 * So no risk or evaluation order ambiguity.
+		 */
 	}
-	printf("%d\n", ceiling);
+	printf("%d\n", end);
 }
 
