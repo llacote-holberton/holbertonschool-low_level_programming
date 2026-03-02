a7e97d696ca899c5afccd84ff9b568157d4e4686
Laurent Lacôte <laurent.lacote@private.com>
2026-03-02 14:24:31 +0100

    C - P/A/S R1 - Exo 00 - Sets given pointed variable to 98.
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      0-main.c 0-reset_to_98.c
      -o 0-98.out

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - forcibly set the value of pointed variable to 98.
+ * @n: pointer to integer
+ * Description: pick the address of a given variable
+ *   to force its value to 98.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Exo 00 */
+void reset_to_98(int *n);
 
 #endif
