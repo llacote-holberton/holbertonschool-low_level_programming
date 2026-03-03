94f932c768f0ca542dbd7b7d1da19a69f060d58c
Laurent Lacôte <laurent.lacote@private.com>
2026-03-03 14:49:34 +0100

    C - P/A/S R1 - Exo 09 (final) - Copying a string
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      9-main.c 9-strcpy.c
      -o 9-strcpy.out
    
    NOTE:
      This version is honestly "too smart" for me xd
      Just kept for reference and explanation on some
      advanced concepts. But I'll rather use the v2
      in real life. ^^

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,38 +11,44 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int cursor;
-
-	/* Can it be as simple as just */
-	/*   reaffecting the target address of dest */
-	/*   to point on the same one as src? */
-	/* NO as exercise requires explicitely to COPY. */
-	cursor = 0;
-
+	char *cursor;
 	/*
-	 * V2 - Smart and concise - Why and how does it work?
-	 * It works because...
-	 * a) Code directly uses the "current cursor index"
-	 *    to affect the value of target cell in source array
-	 *    inside the cell of same index in destination array.
-	 *    Basically we moved the "body of v1 loop"
-	 *      as part of the condition.
-	 * b) Because we put that affectation in parenthesis
-	 *    it becomes an expression to be evaluated.
-	 * c) The "evaluation" of an affectation returns
-	 *     the last value affected, thus the actual value
-	 *     that was just copied.
-	 * d) Because the affectation is made *before*
-	 *     the condition is evaluated once again,
-	 *     on the cycle where cell content of src is '\0'
-	 *     it is affected inside dest as well.
-	 * e) Loop exits cleanly because right *after* this affectation
-	 *     the first part of condition is '\0' so '\0' == '\0' -> TRUE.
-	 * NOTE: this is smart and concise but far less readable for
-	 *   unseasoned devs. Kept for reference as useful in complex codes.
+	 * How does it work?
+	 * It works (ONLY) because of the fixed and specific
+	 *   order in which the C compiler "reads" the line
+	 *   and "processes" the various operators which compose it.
+	 * This uses the following concepts.
+	 * 1/ Deferencement (*pointer_name): telling compiler that we
+	 *   want to access the VALUE held at the address the pointer targets.
+	 * 2/ Affectation returns, which is the value affected
+	 * 3/ Post-incrementation, which is about raising the "value" by 1
+	 *    AFTER having read and returned it in the expression
+	 *    (so only next time it is accessed will it give the updated value).
+	 * 5/ "Lvalue" vs "Rvalue": when encountering an operator of affectation
+	 *    compiler will ALWAYS consider right expression as "VALUE to read"
+	 *    AND left expression as "CONTAINER to put value into".
+	 * 4/ Most importantly operator precedence
+	 *    "accessor" >> ++ (because put on its right) >> * dereference)
+	 *
+	 * So what happens in a cycle
+	 * 1) mypointer++ Returns "current address" THEN increments dest
+	 * 2) "*" targets this "current address" to either...
+	 *    - Give the memory container for that address (if on left of =)
+	 *        AND is a "valid lvalue" (expression which matches a container)
+	 *    - Give the value contained at this address (if on right of =)
+	 * => "Reads the value of cell with "current index" in src, affects it in
+	 *     the cell of "current index" in dest, just before incrementing the index
+	 *     on both sides simultaneously.
+	 *    THEN returns the affected value to evaluate if "should continue?" which
+	 *     will return TRUE for anything beyond 0, '\0' or "false" (<stdbool.h>).
+	 * => Also works cleanly for the last index holding the '\0' BECAUSE
+	 *    it is the result of its affectation itself which stops the loop
+	 * NOTE: we use "cursor" as intermediate to "write over desired memory zone"
+	 *   while keeping the original "starting address" untouched.
 	 */
-	while ((dest[cursor] = src[cursor]) != '\0')
-		cursor++;
+	cursor = dest;
+	while ((*cursor++ = *src++)) /* @note betty requires ; on newline, berk. */
+	;
 	return (dest);
 }
 
@@ -58,4 +64,30 @@ char *_strcpy(char *dest, char *src)
  * }
  * dest[cursor + 1] = '\0';
  * return (dest);
+ */
+
+/*
+ * V2 - Smart and concise - Why and how does it work?
+ * It works because...
+ * a) Code directly uses the "current cursor index"
+ *    to affect the value of target cell in source array
+ *    inside the cell of same index in destination array.
+ *    Basically we moved the "body of v1 loop"
+ *      as part of the condition.
+ * b) Because we put that affectation in parenthesis
+ *    it becomes an expression to be evaluated.
+ * c) The "evaluation" of an affectation returns
+ *     the last value affected, thus the actual value
+ *     that was just copied.
+ * d) Because the affectation is made *before*
+ *     the condition is evaluated once again,
+ *     on the cycle where cell content of src is '\0'
+ *     it is affected inside dest as well.
+ * e) Loop exits cleanly because right *after* this affectation
+ *     the first part of condition is '\0' so '\0' == '\0' -> TRUE.
+ * NOTE: this is smart and concise but far less readable for
+ *   unseasoned devs. Kept for reference as useful in complex codes.
+ * while ((dest[cursor] = src[cursor]) != '\0')
+ * cursor++;*
+ * return (dest);*
  */
