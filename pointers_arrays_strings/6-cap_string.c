10c00c8b1e438339107b79495f0e89e7ee6cda9d
Laurent Lacôte <laurent.lacote@private.com>
2026-03-04 23:35:09 +0100

    C - P/A/S R2 - Exo 06 (v1) - Conditionally Capitalizing.
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      R2_6-main.c 6-cap_string.c
      -o 6-cap_string.out
    
    NOTES: much better than previous version.
    But I don't like the fact I still have
      a nested condition to assess on every character.
    Even though I "know" that I only want to affect
      "the FIRST letter of a word".
    -> Missing link is knowing how to "define"
      that information ("this is the start" or
      "this is the end" of a word).
    Not sure how much performance it would spare though.

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,34 +12,72 @@ char *cap_string(char *string_to_alter)
 {
 	/* Pointer used to parse and hydrate string. */
 	char *cursor;
-	char *separator_checker;
 	/* Set of ASCII characters considered separators */
-	/* Could probably also use ASCII numbers range but */
-	/* Using an explicit set is easier and more readable. */
-	/* ONLY valid way for a oneliner. */
-	/* ACTUALLY not the easiest */
-	/* char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' }; */
-	/* THIS is easier since we get an automatic end of line */
+	/* Using a truth table now that I do how to make one easily in C. */
+	/* Confer https://en.wikipedia.org/wiki/Truth_table. */
+	/* Principle: initialize a table with "falsy values" (0). */
+	/* Size is on purpose: it matches the ASCII (Extended) encoding table. */
+	int separators_table[256] = {0};
+	/* Then we just need to fill in with "truthful values" for separators. */
+	/* Because character is just an ASCII number, we can directly type it  */
+	/*   "as cell index" and C reads its numerical value so no need to know it! */
+	/* Confer https://www.rapidtables.com/code/text/ascii-table.html */
+	/* To avoid "losing" lines "for Betty" we use the "original" string.*/
 	char *separators = " \t\n,;.!?'\"(){}";
+
+	while (*separators != '\0')
+	{
+		/* Type conversion mandatory to avoid compilation warning as error. */
+		separators_table[(unsigned int)*separators] = 1;
+		separators++;
+	}
+	/* THIS is easier since we get an automatic end of line */
 	cursor = string_to_alter;
 	/* @warning CANNOT put the "is lowercase" check at this level */
 	/*   otherwise would (logically) stop at first non-lowercase. */
 	while (*cursor != '\0')
 	{
-		/* printf("Current character is '%c'.\n", *cursor); */
-		/* @note I think it's slightly better to "modify the next" */
-		/*   because otherwise we would "re-check" a character we  */
-		/*   just checked in the previous loop cycle. */
-		/* Maybe easier/faster to just use a massive switchcase? */
-		separator_checker = separators;
-		while (*separator_checker != '\0')
-		{
-			/* printf("  Comparing char %c with separator %c.\n", *cursor, *separator_checker); */
-			if 
+		/* Much simpler than v1 now, but we still have the "nesteed control". */
+		/* 0 is interpreted as false, anything else as true */
+		if (separators_table[(unsigned int)*cursor])
+			if (*(cursor + 1) >= 'a' && *(cursor + 1) <= 'z')
+				*(cursor + 1) = *(cursor + 1) + ('A' - 'a');
+		cursor++;
+	}
+	return (string_to_alter);
+}
+
+/*
+ * V1 VERY DIRTY
+ *
+ * Set of ASCII characters considered separators
+ */
+/* Could probably also use ASCII numbers range but */
+/* Using an explicit set is easier and more readable. */
+/* ONLY valid way for a oneliner. */
+/* ACTUALLY not the easiest */
+/* char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' }; */
+/* THIS is easier since we get an automatic end of line */
+/* char *separators = " \t\n,;.!?'\"(){}"; */
+/* cursor = string_to_alter; */
+/* @warning CANNOT put the "is lowercase" check at this level */
+/*   otherwise would (logically) stop at first non-lowercase. */
+/* while (*cursor != '\0')
+* {
+*/	/* printf("Current character is '%c'.\n", *cursor); */
+	/* @note I think it's slightly better to "modify the next" */
+	/*   because otherwise we would "re-check" a character we  */
+	/*   just checked in the previous loop cycle. */
+	/* Maybe easier/faster to just use a massive switchcase? */
+/*	separator_checker = separators;
+	while (*separator_checker != '\0')
+	{
+		printf("  Comparing char %c with separator %c.\n", *cursor, *separator_checker);
+		if
 			(
 				*cursor == *separator_checker
 				&& *(cursor + 1) >= 'a'
-			  && *(cursor + 1) <= 'z'
+			&& *(cursor + 1) <= 'z'
 			)
 			{
 				*(cursor + 1) = *(cursor + 1) + ('A' - 'a');
@@ -47,9 +85,8 @@ char *cap_string(char *string_to_alter)
 			}
 			else
 				separator_checker++;
-		}
-		cursor++;
-		
 	}
-	return (string_to_alter);
+	cursor++;
 }
+return (string_to_alter);
+*/
