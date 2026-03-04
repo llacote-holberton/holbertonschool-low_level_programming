3e10efa371cdd9231b0b2dde9277c785893c4f4e
Laurent Lacôte <laurent.lacote@private.com>
2026-03-04 14:43:18 +0100

    C - P/A/S R2 - Exo 02 (final) - Copying a string with optional filler
    COMPILER COMMAND
        gcc -Wall -pedantic -Werror -Wextra -std=gnu89
          R2_2-main.c 2-strncpy.c
          -o 2-strncpy.out
    
    NOTE: next time I'll use a whiteboard first
      because I had the general logic from the get go
      but I influenced myself into over-complexity
      with my own description comments. xd

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+/* #include <stdio.h> */
 /**
  * char *_strncpy - copies n chars of 2nd string onto 1st with filling '\0'.
  * @dest: pointer to an array of char (string)
@@ -8,9 +8,9 @@
  *
  * Description: copies up to n chars of src to the end of dest
  *   AND...
- *   0) IF src.length = n 
+ *   0) IF src.length = n
  *   => We includde its EOL character.
- *   1) ELSE IF src.length < n 
+ *   1) ELSE IF src.length < n
  *   => complements any "missing cell" with
  *     null-byte characters to end up with desired length
  *     "dest.updated_length = dest.length + n".
@@ -31,59 +31,33 @@ char *_strncpy(char *dest, char *src, int n)
 	copier = dest;
 	reader = src;
 
-	/*printf("\n\n ORIGINAL version of dest: %s\n", dest);
-	printf("\n\n String to COPY FROM: %s\n", src);*/
-	
-	/* Pushing "cursor" address up to the point where it targets */
-	/* This time we want to OVERWRITE, *not* APPEND */
-	/* while ((*++copier)); */ 
-	/* Using the nice trick I learned from previous exercises. */
-	/* DO NOT forget to add a limit on str size!! xd */
-	/* Otherwise immediate sanction: "stack smashing detected". */
-	/*printf("\nSTART COPY \n");
-	printf("  At start copier address is %p.\n", copier);*/
-	for (char_count = 0; char_count < n; ++char_count)
+	/* ACTUALLY it's much simpler to just separate in two phases */
+	/*   so two distinct loops. */
+	/* While actually makes conditional logic easier to read here imo. */
+	char_count = 0;
+
+	/* @note MUST stop when "meeting" \0 otherwise we go into random address! */
+	/* (reader - src) would work but since we use char_count elsewhere... */
+	while ((char_count) < n && *reader != '\0')
 	{
-		/*printf("Character count is now %d. \n", char_count);*/
-		
-		/* This time we want an "adaptative" behaviour depending on */
-	  /*  the relationship between str.length and n               */
-		/*  so we'll need to "get" its length at some point. */
-		
-		/* if ( *reader == '\0' ) {} */ /* Wrong approach */
-		/* Since character = 1 byte we should get the length */
-		/*   by sustracting address of reader with address of src. */
-		/* 
-		 * Actually (after asking confirmation/explanation) it's the 
-		 *   difference in "number of elements" directly, and work only
-		 *   because we made the two pointers start at the same place (obviously).
-		 */
-		/* STRICT inferiority BECAUSE the nth cycle is the "branching one" */
-		while ( (reader - src) < n )
-		{
-			/*printf("Still copying \n");
-			printf("Before copy reader - src equals %d\n", (int)(reader - src));
-			printf("  Adding character %c \n", *reader); */
-			*copier++ = *reader++;
-			/*printf("AFTER COPY Reader - src equals %d\n", (int)(reader - src));*/
-			++char_count;
-		}
-		/* Nth case management  */
-		/* (I feel it's bad to have this case "inside" the global loop) */
-		if( (reader - src) == n )
-		{
-			/*printf("Processing the special case str length = n \n");
-			printf(" Character at length = n is %c \n", *reader);*/
-			if(*reader == '\0')
-				*copier = *reader;
-		}
-		else
-		{
-			*++copier = '\0';
-		}
+		*copier++ = *reader++;
+		++char_count;
 	}
-	/*printf("Final character count is %d.\n", char_count);
-	printf("\nEND OF COPY\n");
-	printf("New version of dest: %s\n\n", dest);*/
+	/* Nth case management useless since we stop first loop just before now. */
+	/*
+	 * if (*reader == '\0')
+	 * {
+	 *   copier = *reader;
+	 *   ++char_count;
+	 * }
+	 */
+	while (char_count < n)
+	{
+		*copier++ = '\0';
+		++char_count;
+	}
+	/* printf("Final character count is %d.\n", char_count); */
+	/* printf("New version of dest: %s\n\n", dest); */
+
 	return (dest);
 }
