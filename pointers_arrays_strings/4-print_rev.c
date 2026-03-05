d78a2ff983e1db3ca0abe0b521f2b9a81c87b509
Laurent Lacôte <laurent.lacote@private.com>
2026-03-05 15:09:56 +0100

    C - P/A/S R1 - Exo 04 - Trying to fix unknown fail
    No idea what is missing as my code was nearly the same
      as everyone else.
    Only two differences were "no intermediate variable"
      and "do while".
    -> Testing replacing do while with simple while.
    Also adding the previous version as Commented
      to keep the self-learning comments.

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,24 +7,11 @@
  */
 void print_rev(char *s)
 {
-	/*
-	 * To print reverse we must know the length.
-	 * So first must find the length THEN we can just "get pointer and end"
-	 * then decrement to print characters sequentially.
-	 */
+
 	int cursor;
 
 	cursor = 0;
-	/*
-	 * Must first find the length, no way around "reading all".
-	 * BECAUSE '\0' is equated to 0 as a character number
-	 * AND because "0" is evaluated to "false" in conditions
-	 *   it is possible to write while (*string) instead of
-	 *   while (*string != '\0')
-	 * BUT now that we remove the intermediate "length"
-	 * We do NOT want to move "original pointer address" for string
-	 * HENCE HAVE TO use the "dynamic address computation" style.
-	 */
+
 	while (s[cursor])
 	{
 		/* We increment the ADDRESS by 1 which works */
@@ -34,10 +21,10 @@ void print_rev(char *s)
 	/* @IMPORTANT REQUIRED to "remove" the '\0' as it would */
 	/*   otherwise mess the character count (64 expected).  */
 	--cursor;
-	do {
+	while (cursor >= 0)
+	{
 		_putchar(s[cursor]);
 		--cursor;
-	} while (cursor >= 0);
+	}
 	_putchar('\n');
 }
-
---
+++ pointers_arrays_strings/Commented_4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_rev - prints a string with newline on standard output.
+ * @s: pointer to array of char
+ * Description: prints a string followed by newline.
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	/*
+	 * To print reverse we must know the length.
+	 * So first must find the length THEN we can just "get pointer and end"
+	 * then decrement to print characters sequentially.
+	 */
+	int cursor;
+
+	cursor = 0;
+	/*
+	 * Must first find the length, no way around "reading all".
+	 * BECAUSE '\0' is equated to 0 as a character number
+	 * AND because "0" is evaluated to "false" in conditions
+	 *   it is possible to write while (*string) instead of
+	 *   while (*string != '\0')
+	 * BUT now that we remove the intermediate "length"
+	 * We do NOT want to move "original pointer address" for string
+	 * HENCE HAVE TO use the "dynamic address computation" style.
+	 */
+	while (s[cursor])
+	{
+		/* We increment the ADDRESS by 1 which works */
+		/*   because characters take only 1 byte.    */
+		cursor++;
+	}
+	/* @IMPORTANT REQUIRED to "remove" the '\0' as it would */
+	/*   otherwise mess the character count (64 expected).  */
+	--cursor;
+	do {
+		_putchar(s[cursor]);
+		--cursor;
+	} while (cursor >= 0);
+	_putchar('\n');
+}
+
