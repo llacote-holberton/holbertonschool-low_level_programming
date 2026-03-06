f4f309ebf14f22817f563f8d22ef43fecfe81ff3
Laurent Lacôte <laurent.lacote@private.com>
2026-03-06 15:11:59 +0100

    C - P/A/S R1 - Exo 07 - Fixing erreur in setting position.
    Had mixed up length and index so
      decremented without real reason.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,11 @@ void puts_half(char *str)
 	cursor = 0;
 	while (str[cursor])
 		cursor++; /* "Moves temporary pointer" while counting length. */
-	--cursor;  /* Remove the "jump" made for endofstring char '\0'. */
+	/* MUST NOT DEINCREMENT BECAUSE I want the LENGTH and not the "LAST CHAR INDEX" */
+	/* SO since the while stop "when value is 0 and not "after" */
+	/*   cursor is actually equals to the length. */
+	/* Confusion would have not happened with a for i=0 s[i] i++. */
+	/* --cursor;  Remove the "jump" made for endofstring char '\0'. */
 	half_start = (cursor % 2 == 0) ? cursor / 2 : (cursor + 1) / 2;
 	while (str[half_start])
 	{
