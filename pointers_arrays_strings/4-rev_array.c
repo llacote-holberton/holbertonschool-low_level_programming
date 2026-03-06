7f5afd53df42a45f202ad44cf8b2110ef515b0b2
Laurent Lacôte <laurent.lacote@private.com>
2026-03-06 15:24:35 +0100

    C - P/A/S R2 - Exo 04 - Fixing Betty docblock.

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * char reverse_array - reverses a numbers array of length n.
+ * reverse_array - reverses a numbers array of length n.
  * @a: pointer to an array of integers
  * @n: array length which must be provided as no way to know.
  *
  * Description: Takes an array of numbers with its length
  *   and uses it to reverse.
- * Returns: nothing.
+ * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
