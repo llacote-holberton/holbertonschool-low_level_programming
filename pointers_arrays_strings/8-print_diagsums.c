550f2bb5aac2e9268b38c63f8df85f6dc893b51b
Laurent Lacôte <laurent.lacote@private.com>
2026-03-06 15:04:46 +0100

    C - P/A/S R3 - Exo 07 - Printing sums of diagonals.
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      R3_8-main.c 8-print_diagsums.c
      -o 8-print_diagsums.out

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,10 @@
 #include "main.h"
-#define NULL ((void *)0)
 #include <stdio.h>
 
 /**
  * print_diagsums - prints a provided 2d array representing chessboard.
  * @a: pointer to two-dimensional array of numbers of size 8 each dimension.
+ * @size: square side's length.
  *
  * Description: finds the first occurrence of asubstring needle
  *   in the string haystack. The terminating null bytes (\0) are not compared.
@@ -22,10 +22,8 @@ void print_diagsums(int *a, int size)
 {
 	int col_idx;
 	int row_idx;
-	int primary_diagonal = 0;
-	int secondary_diagonal = 0;
-
-	printf("\n\n\nStarting computation.\n");
+	int primary_diagonal_sum = 0;
+	int secondary_diagonal_sum = 0;
 
 	/* Get the sum of primary diagonal */
 	/*
@@ -36,44 +34,35 @@ void print_diagsums(int *a, int size)
 	 *   (index = 2) meaning array[2][2] in flattened it woud be
 	 *   (index * n) + index.
 	 */
-	for (col_idx = 0; col_idx < size;)
+	for (col_idx = 0; col_idx < size; col_idx++)
 	{
-		for (row_idx = 0; row_idx < size; row_idx++)
-		{
-			/*printf("    Adding %d from col %d, row %d \n", a[(col_idx * size + row_idx)], col_idx, row_idx);*/
-			primary_diagonal += a[(col_idx * size + row_idx)];
-			/*printf("       Primary diagonal sum now equals %d.\n", primary_diagonal);*/
-			col_idx++;
-		}
+		/* Useless inner loop since we can compute row index with column one. */
+		/* Technically we could even do without this variable */
+		/*   but I feel it is more readable like this. */
+		row_idx = col_idx;
+		primary_diagonal_sum += a[(col_idx * size + row_idx)];
 	}
-	
+
 	/* For secondary diagonal, we use the mathematical truth  */
 	/*  i + j = n - 1 to deduce the index j value j = n -1 -i */
 	for (col_idx = 0 ; col_idx < size; col_idx++)
 	{
 		row_idx = size - 1 - col_idx;
-		printf("    Adding %d from col %d, row %d \n", a[(col_idx * size + row_idx)], col_idx, row_idx);
-		secondary_diagonal += a[(col_idx * size + row_idx)];
-		printf("       Secondary diagonal sum now equals %d.\n", secondary_diagonal);
-
+		secondary_diagonal_sum += a[(col_idx * size + row_idx)];
 	}
-	printf("   Primary diagonal TOTAL equals %d.\n", primary_diagonal);
-	printf("   Secondary diagonal TOTAL equals %d.\n", secondary_diagonal);
-
+	/* printf("   Primary diagonal TOTAL equals %d.\n", primary_diagonal); */
+	/* printf("   Secondary diagonal TOTAL equals %d.\n", secondary_diagonal); */
+	printf("%d, %d\n", primary_diagonal_sum, secondary_diagonal_sum);
 }
 
 /* ======================== ALGORITHM DETAIL =========================== */
-/* Write a function that prints the sum of the two diagonals of a square matrix of integers.  */
-/* => We must identify which numbers form each of the two diagonals   */
-/* => Then print the sum of each                                                 */
-/* First "primary" diagonal starting frop top left to bottom right is easy to find at least in */
-/*   a perfect square: points have identical coordinates on both dimensions so i = j; */
-/* For the "secondary diagonal" we have to use the mathematical expression which states that  */
-/*   indexes's sum is constant "across the diagonal" and equal to n -1 so i + j = n - 1       */
-/*         */
-/*       */
-/*              */
-/*         */
-/*     */
-/*    */
-/*  */
+/* Write a function that prints the sum of the two diagonals             */
+/*   of a square matrix of integers.  */
+/* => We must identify which numbers form each of the two diagonals      */
+/* => Then print the sum of each                                         */
+/* First "primary" diagonal: from top left to bottom right: easy to find */
+/* at least in a perfect square: points have identical coordinates       */
+/* on both dimensions so i = j; */
+/* For the "secondary diagonal" we have to use mathematical expression   */
+/*   which states that indexes's sum is constant "across the diagonal"   */
+/*   and equal to n -1 so i + j = n - 1                                  */
