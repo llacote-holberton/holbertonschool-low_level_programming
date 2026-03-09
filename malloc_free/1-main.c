0af5113c535427cc6a4a34f00359261811f2368e
Laurent Lacôte <laurent.lacote@private.com>
2026-03-09 13:47:21 +0100

    C - Ma+Fr - R1 - Exo 01 - FIX (NULL str case) and improvements
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      1-main.c 1-strdup.c
      -o 1-strdup.out
    
    NOTES:
    1/ Didn't properly cover the NULL case
      because empty string sill has EOL char
      so length would be 1. And is a "valid"
      use-case by the way.
    => Removed test on length, added a "guard clause"
      on NULL string.
    
    2/ Took the chance to restructure related "main"
      by...
      - putting the edge case "too big for memory"
          in its own function.
      -  writing explicit custom test functions
          for the edge cases "empty" and "NULL".

--- malloc_free/1-main.c
+++ malloc_free/1-main.c
@@ -5,11 +5,13 @@
 #include <sys/resource.h>
 
 /**
- * main - check the code
+ * test_low_memory - checks that the new function
+ *   behaves as expected when asking more memory
+ *   than what is available (must return NULL).
  *
- * Return: Always 0.
+ * Return: Boolean like (0 is success).
  */
-int main(void)
+static int test_low_memory(void)
 {
 	/* Personal addition to test malloc failure */
 	struct rlimit lim;
@@ -18,6 +20,86 @@ int main(void)
 	unsigned int oversized = 100000;
 	unsigned int i;
 	/* End personal addition */
+
+	lim.rlim_cur = 1 * 1024 * 1024;
+	lim.rlim_max = 50 * 1024 * 1024;
+	if (setrlimit(RLIMIT_AS, &lim) == -1)
+		perror("setrlimit");
+	oversized_source = (char *)malloc(oversized);
+	if (!oversized_source)
+	{
+		perror("malloc failed");
+		printf("Couln't check behaviour, not enough memory.\n");
+		return (1);
+	}
+	else
+		for (i = 0; i < oversized; i++)
+			oversized_source[i] = 'a';
+	oversized_copy = _strdup(oversized_source);
+	free(oversized_source);
+	if (oversized_copy != NULL)
+	{
+		printf("Oversized copy happened, was supposed to fail for lack of memory.");
+		return (1);
+	}
+	else
+	{
+		printf("Oversized copy of %d char failed not enough memory! \n", oversized);
+		printf("String duplication behaviour CONFORM for oversized string.\n");
+		return (0);
+	}
+}
+
+/**
+ * test_empty_source - checks that the new function
+ *   behaves as expected when giving empty string
+ *   (should work as it is a valid case).
+ * Return: Boolean like (0 is success).
+ */
+static int test_empty_source(void)
+{
+	char *empty_string = "";
+	char *copy = _strdup(empty_string);
+	int length;
+
+	/* Test case failed, should have copied. */
+	if (copy == NULL)
+		return (1);
+	while (copy[length++] != '\0')
+	;
+	/* Test case failed, should be only 1 char (EOL)*/
+	if (length != 1 || copy[length] != '\0')
+		return (1);
+	printf("String duplication behaviour CONFORM for empty string.\n");
+	return (0);
+}
+
+/**
+ * test_null_source - checks that the new function
+ *   behaves as expected when giving "NULL" source
+ *   (should return NULL as well).
+ * Return: Boolean like (0 is success).
+ */
+static int test_null_source(void)
+{
+	char *null_string = NULL;
+	char *copy = _strdup(null_string);
+
+	if (copy != NULL)
+		return (1);
+	printf("String duplication behaviour CONFORM for NULL string.\n");
+	return (0);
+}
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
 	char *s;
 
 	s = _strdup("Duplicated");
@@ -29,21 +111,10 @@ int main(void)
 	printf("%s\n", s);
 	free(s);
 
-	lim.rlim_cur = 1 * 1024 * 1024;
-	lim.rlim_max = 50 * 1024 * 1024;
-	if (setrlimit(RLIMIT_AS, &lim) == -1)
-		perror("setrlimit");
-	oversized_source = (char *)malloc(oversized);
-	if (!oversized_source)
-	{
-		perror("malloc failed");
-		return (1);
-	}
-	else
-		for (i = 0; i < oversized; i++)
-			oversized_source[i] = 'a';
-	oversized_copy = _strdup(oversized_source);
-	if (oversized_copy == NULL)
-		printf("Oversized copy of %d char failed not enough memory! \n", oversized);
+	/* Personal custom test cases */
+	test_low_memory();
+	test_empty_source();
+	test_null_source();
+
 	return (0);
 }
--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -20,28 +20,41 @@ char *_strdup(char *str)
 	/* Pointer to created copy */
 	char *string_copy;
 
-	/* Fist we get length of original string */
-	/* Note that I assume that it is properly terminated. */
-	/* Approach put away because does not include the EOL char */
-	/* for (length = 0; str[length] != '\0'; length++); */
-	/* We need to use the "while" to have a way to */
-	/*   "integrate" the increment into the condition itself */
-	/*   so we can use the "post-increment" trick */
+	/* Choosing "guard clause" syntax here to stress that */
+	/*   we don't even try to process anything. */
+	if (str == NULL)
+		return (NULL);
+
+	/* This under uses post-increment trick to allow "one more" */
+	/* loop before breaking with condition "current equals EOL" */
 	while (str[length++] != '\0')
 	;
-	/* This above allows "one more loop" before breaking */
-	/* Apparently this works too, interesting */
-	/* for (length = 0; str[length++] != '\0'; */
-	/* printf("Original string is... \n   %s \n ", str); */
+	/* printf("\nOriginal is...\n  %s \n ", (length < 100) ? str : "too big!"); */
 	/* printf("  Its length WITH EOL is %d\n", length); */
-	if (length == 0)
-		string_copy = NULL;
-	else
-		string_copy = (char *)malloc(length * (sizeof(char)));
+
+	string_copy = (char *)malloc(length * (sizeof(char)));
 	if (string_copy != NULL)
 	{
 		for (cpidx = 0; cpidx < length; cpidx++)
 			string_copy[cpidx] = str[cpidx];
+		/* printf("String copy is %s and has length %d \n", string_copy, cpidx); */
 	}
 	return (string_copy);
 }
+
+/* BRAINSTORM / DEV COMMENTS */
+/* Fist we get length of original string */
+/* Note that I assume that it is properly terminated. */
+/* Approach put away because does not include the EOL char */
+/* for (length = 0; str[length] != '\0'; length++); */
+/* We need to use the "while" to have a way to */
+/*   "integrate" the increment into the condition itself */
+/*   so we can use the "post-increment" trick */
+/* Apparently this works too, interesting */
+/* for (length = 0; str[length++] != '\0'; */
+/* WRONG LOGIC!! Doesn't block "empty string" BUT, since */
+/*   empty string has EOL (length = 1) does NOT cover NULL either. */
+/* if (length == 0) */
+/*	string_copy = NULL; */
+/*else */
+/* ... ... ... */
