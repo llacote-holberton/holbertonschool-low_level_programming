4872aab83ac8def33253e7a8057e7778e57a01d1
Laurent Lacôte <laurent.lacote@private.com>
2026-03-09 18:04:55 +0100

    C - Ma+Fr - R1 - Exo 03 (v2) - Creating 2d array of numbers.
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      3-main.c 3-alloc_grid.c
      -o 3-alloc_grid.out
    
    CHANGE: now makes the memory allocation for
      "all table content" in one go, ensuring that
      we don't need to stop the table creation process
      half-way because not enough memory to finish.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-/* @Comment before commit */
-#include <stdio.h>
+
 /**
  * alloc_grid - initializes a 2d array of 0 digits.
  * @width: integer describing width of 2d array.
@@ -15,37 +14,64 @@
 int **alloc_grid(int width, int height)
 {
 	int **integers_table;
+	int *data_area;
 	int i, j; /* Filler iterators */
 	/* Guard clause because invalid "input" for process. */
 	if (!(width > 0) || !(height > 0))
 		return (NULL);
 
 	/* Initializing the first dimension which is "height"! */
-	/* BEWARE of SizeOF's type! NOT 'int' BUT 'int*'   */
 	integers_table = (int **)malloc(height * sizeof(int *));
 	/* Guard against insufficient memory on 1st dimension */
 	if (integers_table == NULL)
 		return (NULL);
-	/* Better to allocate all data space in one go but. */
-	/*   I don't fully understand the logic yet. */
-	/* int *blocks; */
-	/* blocks = (int*)malloc(height * width * sizeof(int)); */
-	/* THIS I understand fully now. */
+
+	/* Better to allocate all data space in one go      */
+	/*   as it avoids the problem of "partial success". */
+	data_area = (int *)malloc(height * width * sizeof(int));
+	if (data_area == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		/* We must create the blocks*/
-		integers_table[i] = (int *)malloc(width * sizeof(int));
-		if (integers_table[i] != NULL)
-			for (j = 0; j < width; j++)
-				integers_table[i][j] = 0;
+		/* The "data_area" is up for grab. */
+		/* The i is used to jump "from one line to another" as we */
+		/*   keep a contiguous "block" for each row of numbers. */
+		/* STILL DOESN'T WORK as we didn't "affect pointers" on columns */
+		/* for (j = 0; j < width; j++) */
+		/* integers_table[i][j] = 0; */
+		/* Define the "starting positions" of each "block" (row), */
+		/*   setting each "first level" pointer address           */
+		/*   with a "jump" (offset) equal to "width of row".      */
+		/* Basically "flattening the coordinates" because memory  */
+		/*   is one-dimensional (just a sequence of 0/1 cells)    */
+		integers_table[i] = data_area + (i * width);
+		/* NOW we can fill it up! 1st levels "know where to strike". */
+		for (j = 0; j < width; j++)
+			integers_table[i][j] = 0;
 	}
-	/* Problem of this is... We can have a "partial table" */
-	/* Initialized for nothing as we "fail at some point". */
-
 	return (integers_table);
 }
 
 /*
+ * === V1 WORKING WITH A FLAW ===
+ * BEWARE of SizeOF's type! NOT 'int' BUT 'int*'
+ * integers_table = (int **)malloc(height * sizeof(int *));
+ * if (integers_table == NULL)
+ *   return (NULL);
+ * for (i = 0; i < height; i++)
+ * {
+ * integers_table[i] = (int *)malloc(width * sizeof(int));
+ * if (integers_table[i] != NULL)
+ *   for (j = 0; j < width; j++)
+ *     integers_table[i][j] = 0;
+ * }
+ * CAN END UP WITH PARTIAL RESULT
+ *   ex try to create 10k size table
+ *   and memory is used up at 6k.
+ * NOT ADVISABLE BEHAVIOUR by default.
+ *
+ * === V0 NON WORKING ===
  * int **new_array;
  * int total_elements;
  * int i, j;
