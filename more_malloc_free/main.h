abff4a1c9b0b130236ffeb30d43ffae82d0c943a
Laurent Lacôte <laurent.lacote@private.com>
2026-03-10 10:31:07 +0100

    C - Ma+Fr - R2 - Preparing custom header with putchar.
    As usual. :)

---
+++ more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Externally provided function defined in _putchar.c */
+int _putchar(char c);
+/* Ma+Fr - R2 - Exo 00 - 0-malloc_checked.c */
+
+/* Ma+Fr - R2 - Exo 01 - 1-string_nconcat.c */
+
+/* Ma+Fr - R2 - Exo 02 - 2-calloc.c */
+
+/* Ma+Fr - R2 - Exo 03 - 3-array_range.c */
+
+
+
+#endif
