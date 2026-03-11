9fd853d01ba7f37917a06c96bf24c66504689b4e
Laurent Lacôte <laurent.lacote@private.com>
2026-03-11 13:35:43 +0100

    C - FnPtrs - Setting up custom header and putchar.

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Externally provided function defined in _putchar.c */
+int _putchar(char c);
+/* FnPtrs - Exo 00 */
+
+/* FnPtrs - Exo 01 */
+
+/* FnPtrs - Exo 02 */
+
+/* FnPtrs - Exo 03 */
+
+
+
+#endif
