09d85321d109582cc2185e54aacce86b08849c66
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 15:05:05 +0100

    C - Struct&Types - Exo 02 - Fixing format for age.
    As in fact example showed that we want
      extreme precision to display age because why not.

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -22,6 +22,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
