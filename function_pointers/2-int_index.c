af38f6ae8544a566ef86ab56a4240d59813000e6
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 16:21:14 +0100

    C - FnPtrs - Exo 02 Rewriting to try and fix.
    CHANGES
    - Removed the match_found variable (I may have
       introduced wrong logic by trying to be smart).
    - Made a direct -1 return if size <=0.
    - Inverted the "main return" logic by returning
      -1 by default.
    - Simplified the "search" logic thanks to removal
        of dedicated variable.
    - Removed the draft function.
    - Removed obsolete comments.

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-/* #include <stdio.h> */
 
 /**
  * int_index - triggers a requested process on an array.
@@ -19,12 +18,11 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int match_idx;   /* Generic iterator index. */
-	int match_found; /* "Truthful value" (>0 is success).  */
+	/* Index of match (case arising) also used as iterator index. */
+	int match_idx;
 
-	match_found = -1;
 	if (size <= 0)
-		return (match_found);
+		return (-1);
 
 	/*
 	 * We want to process an array to find the first element
@@ -37,59 +35,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	match_idx = 0;
 	/* This logic works only as long as we pass functions  */
 	/*   which guarantee passing 0 or negative on failure. */
-	while (match_found <= 0 && match_idx < size)
+	while (match_idx < size)
 	{
-		/* printf("Processing %d of index %d.\n", array[match_idx], match_idx); */
-		match_found = cmp(array[match_idx]);
-		/* printf("Result of comparison is %d.\n", match_found); */
-		if (!(match_found > 0))
-			match_idx++;
+		if (cmp(array[match_idx]) > 0)
+			return (match_idx);
+		match_idx++;
 	}
-	return (match_idx);
-}
-
-
-
-
-/**
- * int_matching_value - triggers a requested process on an array.
- * @array: pointer to array of integers
- * @size: size in integer.
- * @cmp: pointer to a function processing an integer
- *
- * Return: First number found matching the comparison expression.
- *
- */
-int int_matching_value(int *array, int size, int (*cmp)(int))
-{
-	int i;              /* Generic iterator index. */
-	int matched_number; /* Number to return (-1 is no match). */
-	int match_found;    /* "Truthful value" (>0 is success).  */
-	/* Initializing to -1 since "default return" should be "false". */
-	matched_number = -1;
-
-	if (size <= 0)
-		return (matched_number);
-
-	/*
-	 * We want to process an array to find the first element
-	 *   which would match the requested comparison.
-	 * As the array size is given, we can make a loop which
-	 *   should break whichever happens first.
-	 * - Parsed the whole array without finding a match.
-	 * - OR found a match.
-	 */
-	i = 0;
-	/* This logic may bite me if we search -1 as an actual number. */
-	while (matched_number == -1 && i < size)
-	{
-		/* printf("Processing number %d of index %d.\n", array[i], i); */
-		/* YES ACTUALLY IS STUPID BECAUSE SOME FUNCTIONS WILL RETURN 0. */
-		match_found = cmp(array[i]);
-		/* printf("Result of comparison is %d.\n", match_found); */
-		if (match_found > 0)
-			matched_number = array[i];
-		i++;
-	}
-	return (matched_number);
+	return (-1);
 }
