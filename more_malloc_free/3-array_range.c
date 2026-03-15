fb02f60d0be5926f8b8166f54a9665a52afee39b
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 13:55:32 +0100

    C - Ma+Fr - R2 - Exo 03 - Fixing unsupported case
    MISSING CORNER CASE: getting a "sequence size"
      from "deep negative min" and "high positive max".
      Just making the maths as is could end up with a
      "size" exceeding the maximum value of (signed) int.
    => Had to explicitely cast the computation as unsigned
       to prevent weird behaviour from overflow.

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,11 +20,19 @@ int *array_range(int min, int max)
 {
 	int *numbers_sequence;
 	int *sequencer;
+	unsigned int size; /* Required for betty style */
 
 	if (min > max)
 		return (NULL);
+
+	/* Woulnd't have realized the problem without IA help honestly. */
+	/* Problem if not forcing unsigned is: when min is "deep in negative" */
+	/*   while also having max "big enough" since substracting negative" "*/
+	/*   is actually adding, with a regular (signed) int we end up with   */
+	/*   a number far exceeding the maximum value that int can hold. */
+	size = (unsigned int)(max - min + 1);
 	/* Adding one because bounds are included so we must cover min=max. */
-	numbers_sequence = (int *)malloc(sizeof(int) * (max - min + 1));
+	numbers_sequence = (int *)malloc(sizeof(int) * size);
 	if (numbers_sequence == NULL)
 		return (NULL);
 
