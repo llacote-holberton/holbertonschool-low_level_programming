d3489949166eb9e829c0ab33d8aa7c9f1ed82dd3
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 12:41:22 +0100

    C - Ma+Fr - R1 - Exo 03 (v3) - Creating 2d array of numbers
    CHANGE: rewrote the allocation and initialization
      logic for the "inner rows".
    
    NOTE: previous version worked perfectly BUT
      allocated all the space for the "numbers grid"
      in one row.
    Considering how the exercice 04 was formulated
      I guess the checker expected the 03 to make
      the memory allocation and initialization row per row.
    So rewrote as needed.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,46 +14,61 @@
 int **alloc_grid(int width, int height)
 {
 	int **integers_table;
-	int *data_area;
 	int i, j; /* Filler iterators */
-	/* Guard clause because invalid "input" for process. */
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	/* Initializing the first dimension which is "height"! */
 	integers_table = (int **)malloc(height * sizeof(int *));
-	/* Guard against insufficient memory on 1st dimension */
 	if (integers_table == NULL)
 		return (NULL);
 
-	/* Better to allocate all data space in one go      */
-	/*   as it avoids the problem of "partial success". */
-	data_area = (int *)malloc(height * width * sizeof(int));
-	if (data_area == NULL)
-	{
-		free(integers_table);
-		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
-		/* The i is used to jump "from one line to another" as we */
-		/*   keep a contiguous "block" for each row of numbers. */
-		/* STILL DOESN'T WORK as we didn't "affect pointers" on columns */
-		/* for (j = 0; j < width; j++) */
-		/* integers_table[i][j] = 0; */
-		/* Define the "starting positions" of each "block" (row), */
-		/*   setting each "first level" pointer address           */
-		/*   with a "jump" (offset) equal to "width of row".      */
-		/* Basically "flattening the coordinates" because memory  */
-		/*   is one-dimensional (just a sequence of 0/1 cells)    */
-		integers_table[i] = data_area + (i * width);
-		/* NOW we can fill it up! 1st levels "know where to strike". */
+		/* Try to allocate mem for current line */
+		integers_table[i] = (int *)malloc(width * sizeof(int));
+		/* Implies to free all previous if this one fails. */
+		if (integers_table[i] == NULL)
+		{
+			/* Pre-decrement because current i had failed! */
+			while (--i >= 0)
+				free(integers_table[i]);
+			free(integers_table);
+			return (NULL);
+		}
+		/* If row available initialize to 0 */
 		for (j = 0; j < width; j++)
 			integers_table[i][j] = 0;
 	}
 	return (integers_table);
 }
+
+/*
+ * === V2 WORKED PERFECTLY BUT CHECKER
+ *   PROBABLY WANTED A ROW PER ROW AFFECTATION ===
+ * int **integers_table;
+ * int *data_area;
+ * int i, j;
+ * if (width <= 0 || height <= 0)
+ *   return (NULL);
+ * integers_table = (int **)malloc(height * sizeof(int *));
+ * if (integers_table == NULL)
+ *   return (NULL);
+ * data_area = (int *)malloc(height * width * sizeof(int));
+ * if (data_area == NULL)
+ * {
+ *   free(integers_table);
+ *   return (NULL);
+ * }
+ * for (i = 0; i < height; i++)
+ * {
+ *   integers_table[i] = data_area + (i * width);
+ *   for (j = 0; j < width; j++)
+ *     integers_table[i][j] = 0;
+ * }
+ */
 
 /*
  * === V1 WORKING WITH A FLAW ===
