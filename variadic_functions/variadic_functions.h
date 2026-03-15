f9226aa549d32d8f204b04da16e7932c0e451ab0
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 22:05:00 +0100

    C - VarFuncs - Preparing the custom header
    And reintegrating the classic _putchar.

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* Externally provided replacement for putchar. */
+int _putchar(char c);
+/* C - VarFuncs - Exo 00 */
+
+/* C - VarFuncs - Exo 01 */
+
+/* C - VarFuncs - Exo 02 */
+
+/* C - VarFuncs - Exo 03 */
+
+#endif
