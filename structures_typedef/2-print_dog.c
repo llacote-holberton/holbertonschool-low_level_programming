f5fd0a12f7bca8531aa78fb0058a52e8efddc295
Laurent Lacôte <laurent.lacote@private.com>
2026-03-15 14:31:06 +0100

    C - Struct&Types - Exo 02 - Fixing unsupported case
    Which is "no valid pointer provided".
    => Added check that d is not null before printing.

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,5 +13,10 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	/* Same as previous, should always check we do get */
+	/*   an actual pointer to work with. */
+	/* I think however checking pointers for inner members is not */
+	/*   required in context since printf can work with NULL AFAIK. */
+	if (d != NULL)
+		printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
 }
