17e54cb50f232bd5bc4cba45f945777f503ca542
Laurent Lacôte <laurent.lacote@private.com>
2026-03-16 20:05:05 +0100

    C - Recursion - Initializing custom header
    With the classic inclusion of externally
      provided replacement of putchar.

---
+++ recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Externally provided function defined in _putchar.c */
+int _putchar(char c);
+
+/* Recursion - Exo 00 */
+
+/* Recursion - Exo 01 */
+
+/* Recursion - Exo 02 */
+
+/* Recursion - Exo 03 */
+
+/* Recursion - Exo 04 */
+
+/* Recursion - Exo 05 */
+
+/* Recursion - Exo 06 */
+
+
+#endif
