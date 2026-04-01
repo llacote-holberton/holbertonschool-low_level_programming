f2dd6b8fe9a8d9bdcf0adba5a1c36c22dc5bcc44
Laurent Lacôte <laurent.lacote@private.com>
2026-04-01 10:41:46 +0200

    C - DoLiLi - Exo 07 - FIX missing corner cases + REFACTOR logic.
    COMPILER COMMAND
    gcc -Wall -pedantic -Werror -Wextra -std=gnu89
      7-main.c 2-add_dnodeint.c 3-add_dnodeint_end.c
      0-print_dlistint.c 4-free_dlistint.c 7-insert_dnodeint.c
      -o DoLiLi_Exo-07.out
    
    DONE
    - FIXED case leading to segfault (index beyond list).
       Added a condition on loop to properly stop when
       cursor reached end of list (struct pointer NULL).
    - Removed the intermediate variable since we can
       directly use member chaining to reaffect.
    - Reversed the inner loop logic to "continue
       until we reach desired index" since it seemed
       more intuitive to read to me.
    - FIXED improper linking (the n+1 wasn't properly
       linked to the new_node).

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,40 +15,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = NULL;
 	dlistint_t *idx_searcher = NULL;
 	unsigned int tmp_idx = 0;
-	dlistint_t *tmp_previous = NULL;
 
 	/* Pointer must targets a list pointer. */
 	if (h == NULL)
 		return (NULL);
 	/* LIST must not be empty in the first place EXCEPT if target index is 0. */
-	/* SO WE DELEGATE TO OUR custom "add in first place". */
-	/* if (*h == NULL && idx > 0) */
-	/*   return (NULL); */
+	/* Our custom add function does the required checks for it. */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	/* Must be able to "reach" the desired index for insertion. */
 	idx_searcher = *h;
-	while (tmp_idx <= idx)
+	while (tmp_idx <= idx && idx_searcher)
 	{
-		/* Let's be smart, easier (I think) to stop at n-1 */
-		/* EXCEPT it wouldn't cover case "insert at index 0". */
-		if (tmp_idx == idx)
+		/* 0. Push through while we didn't reach index (if exists). */
+		if (tmp_idx != idx)
 		{
-			/* 1. Create new node. */
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				break;
-			new_node->n = n;
-			/* 2. Gets the "previous" */
-			tmp_previous = idx_searcher->prev;
-			/* 3. Link both sides. */
-			tmp_previous->next = new_node;
-			new_node->prev = idx_searcher->prev;
-			new_node->next = idx_searcher;
+			idx_searcher = idx_searcher->next;
+			tmp_idx++;
+			continue;
 		}
-		idx_searcher = idx_searcher->next;
-		tmp_idx++;
+		/* 1. Create new node. */
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			break;
+		/* 2. Affects "internal data". */
+		new_node->n = n;
+		/* 3. Rework the "links" with chaining directly */
+		new_node->next = idx_searcher;
+		new_node->prev = idx_searcher->prev;
+		idx_searcher->prev->next = new_node;
+		idx_searcher->prev = new_node;
+		/* @note order must be exactly this to not lose infos. */
+		break;
 	}
 	return (new_node);
 }
