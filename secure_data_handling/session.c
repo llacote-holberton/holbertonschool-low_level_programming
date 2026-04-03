e2b24157d7d20af54161a1f612c1a5fc26689020
Laurent Lacôte <laurent.lacote@private.com>
2026-04-03 16:56:56 +0200

    C - SeDaTaLab - Resetting session.c to initial state
    No idea where I went wrong so starting fresh (I suppose
      problem comes from somewhere inside session.c because
      project made us work on it first and I fail the first check).

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,84 +2,58 @@
 #include <string.h>
 #include "session.h"
 
-session_t *session_create(const char *id, unsigned int uid,
-													const unsigned char *data, size_t data_len)
+session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
 {
 	session_t *s;
 
 	s = (session_t *)malloc(sizeof(*s));
 	if (!s)
-		return (NULL);
+		return NULL;
 
-	/* s->id = (char *)id; *//* Must be duplicated to avoid ownership ambiguity */
-	s->id = strdup(id);
-	if (!s->id)
-	{
-		free(s);
-		return (NULL);
-	}
+	s->id = (char *)id;
 
 	s->uid = uid;
 
-	if (data_len > 0)
-	{
+	if (data_len > 0) {
 		s->data = (unsigned char *)malloc(data_len);
-		if (!s->data)
-		{
-			/* Must free every other "part of struct" initialized so far */
-			/*   since cannot complete. */
-			free(s->id);
-			free(s);
-			return (NULL);
+		if (!s->data) {
+			return NULL;
 		}
 		memcpy(s->data, data, data_len);
 		s->data_len = data_len;
-	}
-	else
-	{
+	} else {
 		s->data = NULL;
 		s->data_len = 0;
 	}
 
-	return (s);
+	return s;
 }
 
 int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
 {
 	unsigned char *tmp;
 
-	if (!s) /* No use trying to set data if no data given */
-		return (0);
-	if (!data && data_len > 0)
-		return (0);
-	
+	if (!s)
+		return 0;
 
-	if (data_len == 0)
-	{
+	if (data_len == 0) {
 		free(s->data);
 		s->data = NULL;
 		s->data_len = 0;
-		return (1);
+		return 1;
 	}
 
 	tmp = (unsigned char *)realloc(s->data, data_len);
-	/*s->data = tmp;*/ /* Should not allocate directly! */
-	if (!tmp) /* Realloc returned NULL bubble up failure. */
-		return (0);
-	s->data = tmp; /* Reaffects pointer to new/stretched area. */
-	/* BUT data itself is NOT YET updated. */
+	s->data = tmp;
 
 	if (!s->data) {
 		s->data_len = 0;
-		return (0);
+		return 0;
 	}
 
-	/* Copy in X area, from Source, N bytes. */
-	/* Here because we copy strings bytes = elements. */
 	memcpy(s->data, data, data_len);
-	/* Since it was given we can just affect instead of compute. */
 	s->data_len = data_len;
-	return (1);
+	return 1;
 }
 
 void session_destroy(session_t *s)
@@ -88,6 +62,7 @@ void session_destroy(session_t *s)
 		return;
 
 	free(s->id);
+
 	free(s->data);
 	free(s);
 }
