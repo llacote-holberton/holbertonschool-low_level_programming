be89b290727fdc5aaa98a7087fc151a22ffe9b28
Laurent Lacôte <laurent.lacote@private.com>
2026-04-07 09:05:05 +0200

    C - File_IO - Preparing the custom header.

---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* === Externally provided resources === */
+int _putchar(char c);
+/* === Exercises prototypes === */
+
+/* File_IO - Exo 00 - 0-read_textfile.c */
+
+/* File_IO - Exo 01 - 1-create_file.c */
+
+/* File_IO - Exo 02 - 2-append_text_to_file.c */
+
+/* File_IO - Exo 03 - 3-cp.c */
+
+
+
+#endif
